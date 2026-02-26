0feb24d40fffd811a6615b8c2263852092ab4eb6
Filipe Amaro Cinque Santana <97483586+SrCinque@users.noreply.github.com>
2026-02-26 14:16:16 +0000

    fix: ajusta problemas com o TypeScript.

--- pubhub-tcc/src/app/api/v1/user/route.ts
+++ pubhub-tcc/src/app/api/v1/user/route.ts
@@ -2,7 +2,8 @@ import { NextRequest, NextResponse } from "next/server";
 import user from "@services/user";
 
 async function GET(request: NextRequest) {
-    return NextResponse.json({ message: "Hello, World!" });
+  console.log("GET /api/v1/user");
+  return NextResponse.json({ message: "Hello, World!" });
 }
 
 export { GET };
