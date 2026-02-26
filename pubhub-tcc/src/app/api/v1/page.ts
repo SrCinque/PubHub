3ce141cca1555f034ba503af555e7e71d89adf7f
Filipe Amaro Cinque Santana <97483586+SrCinque@users.noreply.github.com>
2026-02-26 14:19:53 +0000

    fix: ajustando rpoblema com page do next

--- pubhub-tcc/src/app/api/v1/page.ts
+++
@@ -1,7 +0,0 @@
-import { NextRequest, NextResponse } from "next/server";
-
-export async function GET(request: NextRequest) {
-
-
-  return '<h1>Olá, esse é um projeto autoral de Filipe Cinque!</h1>' //NextResponse.json({ message: "Hello, World!" });
-}
