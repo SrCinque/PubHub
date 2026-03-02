5d6ca5e1794f0b8ca142c68999ae12de3f29ca18
Filipe Amaro Cinque Santana <97483586+SrCinque@users.noreply.github.com>
2026-03-02 14:45:45 +0000

    fix: alteração de redirecionamento após a criação de usuário.

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -64,7 +64,7 @@ export default function SignUp() {
 
       // Redireciona após 2 segundos
       setTimeout(() => {
-        router.push(`/user/${userData.id}`);
+        router.push(`/`);
       }, 2000);
     } catch (err) {
       setError(err instanceof Error ? err.message : "Erro desconhecido");
